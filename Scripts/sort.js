d102a739091aca1df6a13b5519be2463495bd98e
Tran Diep Phuong <ptrandiep@openwt.com>
2024-04-11 14:26:48 +0700

    Fix bug

--- Scripts/sort.js
+++ Scripts/sort.js
@@ -36,7 +36,7 @@ function sortAlphabetically(a, b) {
     const specialCharsFormat = /[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]+/;
     const textA = a.toUpperCase();
     const textB = b.toUpperCase();
-    if (!specialCharsFormat.test(textA) && specialCharsFormat.test(textB)) {
+    if (!specialCharsFormat.test(textA[0]) && specialCharsFormat.test(textB[0])) {
         return -1;
     } else if (
         specialCharsFormat.test(textA) &&
